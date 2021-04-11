ac205ce0773ea5481d94d8a70bd4d81d41987fa7
Marcel Krizevnik <marcel.krizevnik@gmail.com>
2021-04-11 10:39:45 +0200

    add grain interface

---
+++ GrainInterfaces/GrainInterfaces.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Orleans;
+
+namespace GrainInterfaces
+{
+    public interface IDomainGrain : IGrainWithStringKey
+    {
+        Task<bool> AddEmailAddress(string email);
+        Task<bool> CheckEmailAddress(string email);
+    }
+}
---
+++ Grains/DomainGrainState.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Grains
+{
+    [Serializable]
+    public class DomainGrainState
+    {
+        public HashSet<string> EmailAddresses { get; } = new HashSet<string>();
+    }
+}
